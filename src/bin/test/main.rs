3cfd744b9e4f5ebe4bf548d7cfb5e29a6f99bf0d
Trzyq0712 <j.trzyq@gmail.com>
2023-01-23 14:33:47 +0100

    Add equivalence checker

--- src/bin/test/main.rs
+++ src/bin/test/main.rs
@@ -1,6 +1,8 @@
 use boolean_lang::simplify;
 
 fn main() {
-    let expr = "(~ (~ p))".parse().unwrap();
-    println!("{}", simplify(&expr));
+    // let expr = "(* (~ (+ (~ p) q)) (+ (~ p) r))".parse().unwrap();
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+    println!("{}", simplify(&buf.parse().unwrap()));
 }
--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,29 +1,60 @@
+use std::iter::FromIterator;
+use std::time::Duration;
+
 use egg::*;
 
 define_language! {
-    pub enum BooleanAlgebra {
+    pub enum BooleanLanguage {
         Boolean(bool),
         "+" = Dis([Id; 2]),
         "*" = Con([Id; 2]),
         "~" = Neg(Id),
+        "=>" = Impl([Id; 2]),
+        "<=>" = BiImpl([Id; 2]),
         Symbol(Symbol),
     }
 }
 
-fn boolean_algebra_rules() -> Vec<Rewrite<BooleanAlgebra, ()>> {
-    vec![
+fn boolean_algebra_rules() -> Vec<Rewrite<BooleanLanguage, ()>> {
+    let biderectional = vec![
+        rewrite!("double-negation"; "(~ (~ ?p))" <=> "?p"),
+        rewrite!("ident-disjunction"; "(+ ?p false)" <=> "?p"),
+        rewrite!("ident-conjunction"; "(* ?p true)" <=> "?p"),
+        rewrite!("idemp-disjunction"; "(+ ?p ?p)" <=> "?p"),
+        rewrite!("idemp-conjunction"; "(* ?p ?p)" <=> "?p"),
+        rewrite!("distrib-over-disjunction"; "(* ?p (+ ?q ?r))" <=> "(+ (* ?p ?q) (* ?p ?r))"),
+        rewrite!("distrib-over-conjunction"; "(+ ?p (* ?q ?r))" <=> "(* (+ ?p ?q) (+ ?p ?r))"),
+        rewrite!("demorgan-1"; "(~ (+ ?p ?q))" <=> "(* (~ ?p) (~ ?q))"),
+        rewrite!("demorgan-2"; "(~ (* ?p ?q))" <=> "(+ (~ ?p) (~ ?q))"),
+        rewrite!("implication"; "(=> ?p ?q)" <=> "(+ (~ ?p) ?q)"),
+        rewrite!("biimplication"; "(<=> ?p ?q)" <=> "(* (=> ?p ?q) (=> ?q ?p))"),
+    ]
+    .concat();
+    let directional = vec![
         rewrite!("commute-disjunction"; "(+ ?p ?q)" => "(+ ?q ?p)"),
         rewrite!("commute-conjunction"; "(* ?p ?q)" => "(* ?q ?p)"),
-        rewrite!("double-negation"; "(~ (~ ?p))" => "?p"),
-    ]
+        rewrite!("assoc-disjunction"; "(+ ?p (+ ?q ?r))" => "(+ (+ ?p ?q) ?r)"),
+        rewrite!("assoc-conjunction"; "(* ?p (* ?q ?r))" => "(* (* ?p ?q) ?r)"),
+        rewrite!("annihil-disjunction"; "(+ ?p true)" => "true"),
+        rewrite!("annihil-conjunction"; "(* ?p false)" => "false"),
+        rewrite!("absorption-1"; "(* ?p (+ ?p ?q))" => "?p"),
+        rewrite!("absorption-2"; "(+ ?p (* ?p ?q))" => "?p"),
+        rewrite!("complement-1"; "(+ ?p (~ ?p))" => "true"),
+        rewrite!("complement-2"; "(* ?p (~ ?p))" => "false"),
+    ];
+
+    Vec::from_iter(biderectional.into_iter().chain(directional))
 }
 
-pub fn simplify(expr: &RecExpr<BooleanAlgebra>) -> RecExpr<BooleanAlgebra> {
+pub fn simplify(expr: &RecExpr<BooleanLanguage>) -> RecExpr<BooleanLanguage> {
     // parse the expression, the type annotation tells it which Language to use
 
     // simplify the expression using a Runner, which creates an e-graph with
     // the given expression and runs the given rules over it
     let runner = Runner::default()
+        .with_iter_limit(100)
+        .with_time_limit(Duration::from_secs(120))
+        .with_node_limit(30_000)
         .with_expr(expr)
         .run(&boolean_algebra_rules());
 
@@ -33,18 +64,45 @@ pub fn simplify(expr: &RecExpr<BooleanAlgebra>) -> RecExpr<BooleanAlgebra> {
     // use an Extractor to pick the best element of the root eclass
     let extractor = Extractor::new(&runner.egraph, AstSize);
     let (best_cost, best) = extractor.find_best(root);
-    println!("Simplified {expr} to {best} with cost {best_cost}");
+    println!("Simplified {expr} \nto {best} \nwith cost {best_cost}");
     best
 }
+
+pub fn check_equivalence(
+    expr_a: &RecExpr<BooleanLanguage>,
+    expr_b: &RecExpr<BooleanLanguage>,
+) -> bool {
+    let runner = Runner::default()
+        .with_iter_limit(100)
+        .with_time_limit(Duration::from_secs(120))
+        .with_node_limit(30_000)
+        .with_expr(expr_a)
+        .with_expr(expr_b)
+        .run(&boolean_algebra_rules());
+
+    !runner.egraph.equivs(expr_a, expr_b).is_empty()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    fn expr(s: &str) -> RecExpr<BooleanLanguage> {
+        s.parse().unwrap()
+    }
+
     #[test]
     fn double_negation() {
-        let expr = "(~ (~ p))".parse().unwrap();
-        let simplified = simplify(&expr);
-        let expected = "p".parse().unwrap();
+        let e = expr("(~ (~ p))");
+        let simplified = simplify(&e);
+        let expected = expr("p");
         assert_eq!(simplified, expected);
+    }
+
+    #[test]
+    fn check_contains() {
+        let complex = expr("(* (~ (=> p q)) (=> p r))");
+        let simplified = expr("(* p (* (~ q) r))");
+        assert!(check_equivalence(&complex, &simplified))
     }
 }
